c8d9de56f828f885a034b7b40c271b27dbd6ba1d
= <=>
2023-01-31 11:41:41 +0530

    Aded Sorting on Array List Program

---
+++ Programs/A21/CompareExp.java
@@ -0,0 +1,25 @@
+package sorting_ArrayList;
+
+import java.util.*;
+// comparator interface is stored in java.util
+
+public class CompareExp implements Comparator<Employee2>{
+	/* When Collection sorts an ArrayList
+	 * it calls the compare function for the syntax
+	 * 		Collections.sort(arraylist , helper class) 
+	 * 		here, while sorting, it calls compare function to see which object is greater
+	 * 		for compare(obj1,obj2)
+	 * 		if obj1 > obj2 then return positive value,ie, if we are sorting on age, if obj1.age > obj2.age the return any positive number  
+	 * 		if obj1 < obj2 the return a negative value,ie, if we are sorting on age, if obj1.age < obj2.age the return any negative number
+	 * 		if both objects have same sorting value then return 0,ie, if we are sorting on age, if obj1.age = obj2.age the return 0  
+	 */
+	public int compare(Employee2 E1,Employee2 E2)
+	{
+		if(E1.xp>E2.xp)
+			return 1;
+		if(E1.xp<E2.xp)
+			return -1;
+		return 0;
+	}
+
+}
